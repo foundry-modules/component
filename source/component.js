8f2e96a2131c593fa343cabb9dc7a95b9cc7a338
Jason Ramos <jason@stackideas.com>
2012-02-20 12:45:12 +0800

    Updated component to work with module & newer version of require.

---
+++ source/component.js
@@ -0,0 +1,106 @@
+/**
+ * jquery.component.
+ * Boilerplate for client-side MVC application.
+ *
+ * Copyright (c) 2011 Jason Ramos
+ * www.stackideas.com
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+var Components = {};
+
+var Component = function(name, options, callback) {
+
+    var self = this;
+
+    self.options       = options;
+    self.name          = name;
+    self.componentName = "com_" + this.name.toLowerCase();
+    self.version       = options.version;
+    self.environment   = options.environment || Foundry.environment;
+    self.debug         = (self.environment=='development');
+    self.baseUrl       = options.baseUrl || Foundry.indexUrl + "?option=" + this.componentName;
+    self.scriptPath    = options.scriptPath || Foundry.rootPath + "media/" + this.componentName + (self.debug) ? "/scripts_/" : "/scripts/";
+    self.ejsPath       = self.baseUrl + '&controller=themes&task=getAjaxTemplate&no_html=1&tmpl=component&templateName=';
+    self.isReady       = false;
+    self.dependencies  = $.Deferred();
+
+    var resolveComponent = function() {
+
+        self.dependencies.resolve();
+
+        self.ready(function() {
+            self.isReady = true;
+            self.run(callback);
+        });
+    }
+
+    // Load component dependencies,
+    if ($.isFunction(options.dependencies)) {
+
+        var require = self.require({loadingComponentDependencies: true});
+
+        options.dependencies.call(self, require);
+
+        require.done(resolveComponent);
+
+    // or resolve component straightaway.
+    } else {
+        resolveComponent();
+    }
+}
+
+$.extend(Component.prototype, {
+
+    run: function(func) {
+
+        if (!$.isFunction(func))
+            return;
+
+        return func.call(self, $);
+    },
+
+    ready: function(callback) {
+
+        if (!$.isFunction(callback))
+            return;
+
+        var self = this;
+
+        self.dependencies
+            .done(function() {
+                $.module('component/mvc').done(callback);
+            });
+    },
+
+    require: function(options) {
+
+        var self = this,
+            require = $.require($.extend(options, {path: self.scriptPath})),
+            done = require.done;
+
+        // To ensure all require callbacks are executed after the component's dependencies are ready,
+        // every callback made through component.require() is wrapped in a component.ready() function.
+        require.done = function(callback) {
+            done.call(null, (options.loadingComponentDependencies) ? callback : function() { self.ready(callback); });
+        }
+
+        return require;
+    }
+});
+
+$.Component = function(name, options, callback) {
+
+    if (arguments.length < 1)
+        return Components;
+
+    if (arguments.length < 2)
+        return Components[name];
+
+    // Create a global namespace for this component
+    return window[name] = Components[name] = new Component(name, options, callback);
+};
---
+++ source/component.mvc.js
@@ -0,0 +1,111 @@
+$.module('component/mvc', function() {
+
+var module = this;
+
+$.require()
+    .script(
+        'server',
+        'mvc/controller',
+        'mvc/model',
+        'mvc/view',
+        'mvc/lang.json'
+    )
+    .done(function() {
+
+        Component.prototype.ajax = function(namespace, params, callback) {
+            var self = this;
+
+            var options = {
+                    url: Foundry.indexUrl,
+                    data: $.extend(
+                        params,
+                        {
+                            option: self.componentName,
+                            namespace: namespace
+                        }
+                    )
+                };
+
+            options = $.extend(true, options, this.options.ajax);
+
+            // This is for server-side function arguments
+            if (options.data.hasOwnProperty('args'))
+                options.data.args = $.toJSON(options.data.args);
+
+            if (callback.type=='jsonp')
+            {
+                delete callback.type;
+
+                callback.dataType = 'jsonp';
+
+                // This ensure jQuery doesn't use XHR should it detect the ajax url is a local domain.
+                callback.crossDomain = true;
+            }
+
+            if ($.isPlainObject(callback))
+                $.extend(options, callback);
+
+            if ($.isFunction(callback))
+                options.success = callback;
+
+            return $.server(options);
+        };
+
+        Component.prototype.Controllers = function() {
+            var args = arguments;
+            args[0] = this.name + '.Controllers.' + args[0];
+
+            if (args.length < 2)
+            {
+                return $.String.getObject(args[0]);
+            };
+
+            // TODO: Destroy controller function
+            this.Controllers.destroy = function(){};
+
+            return $.Controller.apply(this, args);
+        };
+
+        // TODO: Setter.
+        Component.prototype.Views = function(name) {
+
+            // Append component identifier
+            name = this.name + '.Views.' + name;
+            name = name.toLowerCase();
+
+            // If template does not exist in <script> tag, use the url.
+            if ($('script[type="text/ejs"][id="'+name+'"]').length < 1)
+            {
+                name = self.ejsPath + name;
+            };
+
+            // Replace the original argument
+            var args = $.makeArray(arguments);
+            args[0] = name;
+
+            return $.View.apply(this, args);
+        };
+
+        // Depecrated.
+        Component.prototype.View = function(name) {
+            // Append component identifier
+            name = this.name.toLowerCase() + '.' + name;
+
+            // If template does not exist in <script> tag, use the url.
+            if ($('script[type="text/ejs"][id="'+name+'"]').length < 1)
+            {
+                name = self.ejsPath + name;
+            };
+
+            // Replace the original argument
+            var args = $.makeArray(arguments);
+            args[0] = name;
+
+            return $.View.apply(this, args);
+        };
+
+        module.resolve();
+
+    });
+
+});
