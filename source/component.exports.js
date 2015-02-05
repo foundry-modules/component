587c8da4cc73ccf181926b6d4685f5c09632fe36
Mark Lee <mark@stackideas.com>
2015-02-06 00:02:24 +0800

    Component registration is done via bootloader instead

--- source/component.exports.js
+++ source/component.exports.js
@@ -2,13 +2,3 @@
 
 // Execute all pending foundry modules
 %BOOTCODE%.module.execute();
-
-// Get all abstract components
-$.each(%BOOTCODE%.component(), function(i, abstractComponent){
-
-    // If this component is registered, stop.
-    if (abstractComponent.registered) return;
-
-    // Create an instance of the component
-    $.Component.register(abstractComponent);
-});
